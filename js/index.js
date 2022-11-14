3aba5433b9fdeb17684a83ef4be2328c5c1c464b
Iker-hub <ikergargu@gmail.com>
2022-11-14 18:49:55 +0100

    workin on the animations

--- js/index.js
+++ js/index.js
@@ -5,16 +5,31 @@ grapichInterface.init({
   id: "idForm",
   components: [
     {
-      name: "button",
-      id: "idButton1",
+      type: "checkbox",
+      id: "idCheckbox",
+      label: "Tamagochi",
+      value: "valueCheckbox",
+      listener: null,
     },
     {
-      name: "button",
-      id: "idButton2",
+      type: "button",
+      id: "idButton",
+      value: "Feed",
+      listener: listenerButton,
     },
     {
-      name: "input",
-      id: "idInput",
+      type: "range",
+      id: "idRange",
+      label: "Quantity of food",
+      min: "0",
+      max: "100",
+      step: "10",
+      value: "50",
+      listener: null,
     },
   ],
 });
+
+function listenerButton() {
+  console.log("hola");
+}
--- js/interface.js
+++ js/interface.js
@@ -1,20 +1,88 @@
+import anime from "../js/anime.es.js";
+
 class GrapichInterface {
   init(configuration) {
     this.defineForm(configuration.id);
-    this.defineComponents(configuration.components);
+    this.defineComponents(configuration);
   }
 
   defineForm(id) {
     let form = document.createElement("form");
-    form.setAttribute("id", id);
+    form.id = id;
     document.body.appendChild(form);
   }
 
-  defineComponents(components) {
-    for (const component of components) {
-      let item = document.createElement(component.name);
-      item.setAttribute("id", component.id);
-      document.body.appendChild(item);
+  defineComponents(config) {
+    config.components.forEach((component) => {
+      let item = document.createElement("input");
+      item.type = component.type;
+      item.id = component.id;
+
+      switch (item.type) {
+        case "checkbox":
+          this.defineCheckbox(config, component, item);
+          break;
+        case "button":
+          this.defineButton(component, item);
+          break;
+        case "range":
+          this.defineRange(config, component, item);
+          break;
+      }
+
+      document.getElementById(config.id).appendChild(item);
+    });
+  }
+
+  defineCheckbox(config, component, item) {
+    if (component.label != null) {
+      let label = document.createElement("label");
+      label.for = component.id;
+      label.appendChild(document.createTextNode(component.label));
+      document.getElementById(config.id).appendChild(label);
+    }
+
+    if (component.value != null) {
+      item.value = component.value;
+    }
+
+    if (component.listener != null) {
+      item.addEventListener("click", component.listener);
+    }
+  }
+
+  defineButton(component, item) {
+    if (component.value != null) {
+      item.value = component.value;
+    }
+
+    if (component.listener != null) {
+      item.addEventListener("click", component.listener);
+    }
+  }
+
+  defineRange(config, component, item) {
+    if (component.label != null) {
+      let label = document.createElement("label");
+      label.for = component.id;
+      label.appendChild(document.createTextNode(component.label));
+      document.getElementById(config.id).appendChild(label);
+    }
+
+    if (component.min != null) {
+      item.min = component.min;
+    }
+
+    if (component.max != null) {
+      item.max = component.max;
+    }
+
+    if (component.step != null) {
+      item.step = component.step;
+    }
+
+    if (component.value != null) {
+      item.value = component.value;
     }
   }
 }
